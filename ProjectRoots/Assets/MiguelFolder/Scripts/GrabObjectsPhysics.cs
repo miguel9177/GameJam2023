5294016571bb84e0bdafbfd16b83a5833d1ce56e
miguel9177 <luis2.128.b80@gmail.com>
2023-02-03 19:29:31 +0000

    made object be able to be grabbed

--- ProjectRoots/Assets/MiguelFolder/Scripts/GrabObjectsPhysics.cs
+++ ProjectRoots/Assets/MiguelFolder/Scripts/GrabObjectsPhysics.cs
@@ -1,8 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GrabObjectsPhysics : MonoBehaviour
 {
-    
+    private enum State { idle, GrabbingObject}
+
+    [Header("Components")]
+    public Rigidbody grabableObjectRigidBody;
+    public Collider grabableObjectCollider;
+    public Rigidbody playerRigidBody;
+    public Collider playerCollider;
+    public Transform parentOfGrabbedObjects;
+
+    [Header("Data")]
+    private State state;
+
+    private void Start()
+    {
+        InputManager.Instance.OnPressedE += OnPressedE;
+    }
+
+    private void Update()
+    {
+        CallFunctionsDependingOnState();
+    }
+
+    private void CallFunctionsDependingOnState()
+    {
+        switch(state)
+        {
+            case State.idle:
+                break;
+            case State.GrabbingObject:
+                GrabbingObjectPhysicsUpdate();
+                break;
+        }
+    }
+
+    private void OnPressedE()
+    {
+        if(state == State.GrabbingObject)
+        {
+            DropGrabedObject();
+        }
+        else
+        {
+            GrabObject();
+        }
+    }
+
+    private void GrabObject()
+    {
+        state = State.GrabbingObject;
+        Physics.IgnoreCollision(grabableObjectCollider, playerCollider, true);
+    }
+
+    private void DropGrabedObject()
+    {
+        state = State.idle;
+        Physics.IgnoreCollision(grabableObjectCollider, playerCollider, false);
+    }
+
+    private void GrabbingObjectPhysicsUpdate()
+    {
+        //grabableObject.transform.parent = parentOfGrabbedObjects.transform;
+        grabableObjectRigidBody.transform.localPosition = parentOfGrabbedObjects.position;
+        grabableObjectRigidBody.transform.localRotation = parentOfGrabbedObjects.rotation;
+    }
 }
