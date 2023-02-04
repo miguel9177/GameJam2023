b8a5210b0ee2ffe293ded804b5abaf559e4966db
Tiago Filipe Madeira Patricio (filipemadt) <tiagopatricio3000@gmail.com>
2023-02-04 05:54:48 +0000

    New HouseAddition and scene change

---
+++ ProjectRoots/Assets/tiago stuff/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+   public float speed;
+   public Rigidbody RB;
+    Vector2 moveDir;
+    Vector3 lookDir;
+    public Camera camera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        lookDir = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse Z")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        RB.velocity = new Vector3(moveDir.x * speed, RB.velocity.y, moveDir.y * speed);
+       camera.transform.rotation= new Quaternion(lookDir.x, lookDir.y, lookDir.z, camera.transform.rotation.w);
+    }
+}
